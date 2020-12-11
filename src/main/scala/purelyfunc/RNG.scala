10cc315d299410bdde56fc9d7310372383406628
Roozbeh Khodadadeh <roozbeh.khd@powerdata.com>
2020-12-11 14:49:15 -0800

    Section 6.2 exercises done

--- src/main/scala/purelyfunc/RNG.scala
+++ src/main/scala/purelyfunc/RNG.scala
@@ -2,8 +2,8 @@ package purelyfunc
 
 trait RNG {
   def nextInt : (Int, RNG)
+  def nextDouble : (Double, RNG)
 }
-
 
 case class SimpleRNG(seed : Long) extends RNG {
   override def nextInt: (Int, RNG) = {
@@ -11,5 +11,31 @@ case class SimpleRNG(seed : Long) extends RNG {
     val nextRNG = SimpleRNG(newSeed)
     val n = (newSeed >>> 16).toInt
     (n, nextRNG)
+  }
+  override def nextDouble : (Double, RNG) = {
+    val (a, nextRNG) = nextInt
+    (a.toDouble / Int.MaxValue, nextRNG)
+  }
+}
+
+object RNG {
+  def intDouble(rng : RNG) : ((Int, Double), RNG) = {
+    val (i, rng2) = rng.nextInt
+    val (d, rng3) = rng2.nextDouble
+    ((i, d), rng3)
+  }
+
+  def ints(count : Int)(rng : RNG) : (List[Int], RNG) = {
+    (0 until count).foldLeft((List.empty[Int], rng)) ((a, b) => {
+      val (i, nrng) = rng.nextInt
+      (a._1 :+ i, nrng)
+    })
+  }
+  def main(args: Array[String]): Unit = {
+    val rng = SimpleRNG(42)
+    println(ints(4)(rng))
+//    val x : Rand[Int] = _.nextInt
+//    println(x(rng))
+
   }
 }
